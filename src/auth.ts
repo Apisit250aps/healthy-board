50124377e02c0168e17bf044caf0be80ffefa012
U <88958116+Apisit250aps@users.noreply.github.com>
2026-05-22 19:56:01 +0700

    Add MongoDB integration with NextAuth and update dependencies

---
+++ src/auth.ts
@@ -0,0 +1,9 @@
+import NextAuth from 'next-auth'
+import Google from 'next-auth/providers/google'
+import { MongoDBAdapter } from '@auth/mongodb-adapter'
+import client from './db'
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: MongoDBAdapter(client),
+  providers: [Google],
+})
